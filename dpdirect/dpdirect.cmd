444ebcbfdf8aed106b7592b2f204872946206ff5
SysInt Australia <mqsysadmin@gmail.com>
2020-10-15 14:17:51 +1100

    Update dpdirect.cmd

--- dpdirect/dpdirect.cmd
+++ dpdirect/dpdirect.cmd
@@ -1,2 +1,2 @@
 @echo off
-java -cp .\dpdirect-1.0.9.jar;.\lib\log4j.jar;.\lib\xercesImpl.jar;.\lib\xml-apis.jar org.dpdirect.dpmgmt.DPDirect DEV %*
+java -cp .\dpdirect-1.0.9.jar;.\lib\log4j.jar;.\lib\xercesImpl.jar;.\lib\xml-apis.jar org.dpdirect.dpmgmt.DPDirect %*
